95ede1e25e280083b887986db52e06b0c6117041
Svarecka37 <maty.piernik@gmail.com>
2025-09-17 17:36:56 +0200

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Maty Pierník on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
